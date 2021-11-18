aa0857566bd24fadee263be2c8d728c412584f7c
Isamu <>
2021-11-18 15:56:38 -0500

    adding force for casket

--- HilltopFuneral/Assets/Player1.cs
+++ HilltopFuneral/Assets/Player1.cs
@@ -13,82 +13,126 @@ public class Player1 : MonoBehaviour
     public GameObject casket;
     public GameObject player2;
     // Start is called before the first frame update
+
+
+    // Body
+    public Rigidbody rigidbody;
+    private Rigidbody casketBody;
+
+    // Movement
+    Vector3 moveAmount;
+    Vector3 smoothMoveVelocity;
+    public float walkSpeed = 6;
+    private float speed;
+
+    //Pull
+    public float pullForce = -5f;
+
     void Start()
     {
         casket = GameObject.Find("Casket");
         player2 = GameObject.Find("Player2");
+
+
+
+        casketBody = casket.GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(foundCas){
-            GetComponent<Rigidbody>().drag = 10;
-        }
-        else{
-            GetComponent<Rigidbody>().drag = 5;
-        }
 
-        if (Input.GetKey(KeyCode.UpArrow)){
-            if(foundCas){
-                lift = true;
-            }
-            GetComponent<Rigidbody>().AddForce(Vector3.forward * forceForward);
-            if(lift && player2.GetComponent<Player2>().lift){
-                Vector3 relativePos = transform.position - casket.transform.position;
-                casket.GetComponent<Rigidbody>().AddForce(relativePos);
-            }
 
-        }
+        //if(foundCas){
+        //    GetComponent<Rigidbody>().drag = 10;
+        //}
+        //else{
+        //    GetComponent<Rigidbody>().drag = 5;
+        //}
 
-        if (Input.GetKey(KeyCode.LeftArrow)){
+        //if (Input.GetKey(KeyCode.UpArrow)){
+        //    if(foundCas){
+        //        lift = true;
+        //    }
+        //    GetComponent<Rigidbody>().AddForce(Vector3.forward * forceForward);
+        //    if(lift && player2.GetComponent<Player2>().lift){
+        //        Vector3 relativePos = transform.position - casket.transform.position;
+        //        casket.GetComponent<Rigidbody>().AddForce(relativePos);
+        //    }
 
-            GetComponent<Rigidbody>().AddForce(Vector3.left * forceHorizontal);
-            if(lift && player2.GetComponent<Player2>().lift){
-                Vector3 relativePos = transform.position - casket.transform.position;
-                casket.GetComponent<Rigidbody>().AddForce(relativePos/2);
-            }
-        }
-        if (Input.GetKey(KeyCode.RightArrow)){
-            GetComponent<Rigidbody>().AddForce(Vector3.right * forceHorizontal);
-            if(lift && player2.GetComponent<Player2>().lift){
-                Vector3 relativePos = transform.position - casket.transform.position;
-                casket.GetComponent<Rigidbody>().AddForce(relativePos/2);
-            }
-        }
-        if (Input.GetKey(KeyCode.DownArrow)){
-            GetComponent<Rigidbody>().AddForce(-Vector3.forward * forceForward);
-        }
+        float inputX = Input.GetAxisRaw("Horizontal2");
+        float inputY = Input.GetAxisRaw("Vertical2");
 
-        if(Input.GetKeyUp(KeyCode.UpArrow)){
-            lift = false;
-        }
+        Vector3 moveDir = new Vector3(inputX, 0, inputY).normalized;
+        Vector3 targetMoveAmount = moveDir * speed;
+        moveAmount = Vector3.SmoothDamp(moveAmount, targetMoveAmount, ref smoothMoveVelocity, .15f);
+
+        // Check ground
+        Ray ray = new Ray(transform.position, -transform.up);
+        RaycastHit hit;
+
+        //}
+
+        //if (Input.GetKey(KeyCode.LeftArrow)){
+
+        //    GetComponent<Rigidbody>().AddForce(Vector3.left * forceHorizontal);
+        //    if(lift && player2.GetComponent<Player2>().lift){
+        //        Vector3 relativePos = transform.position - casket.transform.position;
+        //        casket.GetComponent<Rigidbody>().AddForce(relativePos/2);
+        //    }
+        //}
+        //if (Input.GetKey(KeyCode.RightArrow)){
+        //    GetComponent<Rigidbody>().AddForce(Vector3.right * forceHorizontal);
+        //    if(lift && player2.GetComponent<Player2>().lift){
+        //        Vector3 relativePos = transform.position - casket.transform.position;
+        //        casket.GetComponent<Rigidbody>().AddForce(relativePos/2);
+        //    }
+        //}
+        //if (Input.GetKey(KeyCode.DownArrow)){
+        //    GetComponent<Rigidbody>().AddForce(-Vector3.forward * forceForward);
+        //}
+
+        //if(Input.GetKeyUp(KeyCode.UpArrow)){
+        //    lift = false;
+        //}
 
 
 
     }
 
-    void OnCollisionEnter(Collision coll){
-        if (coll.gameObject.name == "Casket"){
-            foundCas = true;
+
+    public void AttractCasket()
+    {
+        Vector3 vectorTo = (casketBody.position - transform.position).normalized;
+        float casketDist = Vector3.Distance(casketBody.position, transform.position);
+
+        if (Mathf.Abs(casketDist) < 5)
+        {
+            casketBody.AddForce(vectorTo * pullForce);
         }
     }
 
-    // void OnCollisionExit(Collision coll){
-    //     if (coll.gameObject.name == "Casket"){
-    //         foundCas = false;
-    //     }
-    // }
+    //void OnCollisionEnter(Collision coll){
+    //    if (coll.gameObject.name == "Casket"){
+    //        foundCas = true;
+    //    }
+    //}
 
-    void OnTriggerEnter(Collider coll){
-        if (coll.gameObject.tag == "Casket"){
-            foundCas = true;
-        }
-    }
+    //// void OnCollisionExit(Collision coll){
+    ////     if (coll.gameObject.name == "Casket"){
+    ////         foundCas = false;
+    ////     }
+    //// }
 
-    void OnTriggerExit(Collider coll){
-        if (coll.gameObject.tag == "Casket"){
-            foundCas = false;
-        }
-    }
+    //void OnTriggerEnter(Collider coll){
+    //    if (coll.gameObject.tag == "Casket"){
+    //        foundCas = true;
+    //    }
+    //}
+
+    //void OnTriggerExit(Collider coll){
+    //    if (coll.gameObject.tag == "Casket"){
+    //        foundCas = false;
+    //    }
+    //}
 }
