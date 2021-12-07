a6b2304d9f2609edf54dc29cd1f1669dc212293a
Luke <yiyanl@uci.edu>
2021-12-07 14:44:26 -0500

    Nothing

--- HilltopFuneral/Assets/GameManager.cs
+++ HilltopFuneral/Assets/GameManager.cs
@@ -28,9 +28,7 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.R)){
-            Application.LoadLevel(Application.loadedLevel);
-        }
+        
     }
 
 
