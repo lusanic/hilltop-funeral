779e981ebddb5ee13e420ddc53cd746890dbd3fa
Haijing Lin <hl4540@gmail.com>
2021-12-08 23:10:58 -0500

    textured terrain + assets

--- HilltopFuneral/Assets/GameManager.cs
+++ HilltopFuneral/Assets/GameManager.cs
@@ -3,14 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public static class GameState
-{
-    public static int life = 3;
-    public static bool onGround = false;
-
-
-}
-
 public class GameManager : MonoBehaviour
 {
     private string name;
@@ -21,55 +13,22 @@ public class GameManager : MonoBehaviour
     public GameObject nameInput;
     public GameObject startButton;
 
-    public GameObject deathCanvas;
-
     public GameObject player1;
     public GameObject player2;
-    public GameObject p1dialogue;
-    public GameObject p2dialogue;
 
     public GameObject casket;
     public GameObject deadBody;
-    public GameObject anchor;
-
-    public int level = 1;
-
-    public Vector3 p1Pos;
-    public Vector3 p2Pos;
-    public Vector3 casPos;
-    public Vector3 bodyPos;
-    public Vector3 anchorPos;
 
     // Start is called before the first frame update
     void Start()
     {
         date =  System.DateTime.Now.ToString("MM/dd");
-        p1Pos = player1.transform.position;
-        p2Pos = player2.transform.position;
-        casPos = casket.transform.position;
-        anchorPos = anchor.transform.position;
-        bodyPos = deadBody.transform.GetChild(1).position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(GameState.onGround){
-            handleDropping();
-        }
-    }
-
-    public void handleDropping(){
-        GameState.life = GameState.life-1;
-        player1.transform.LookAt(deadBody.transform);
-        player2.transform.LookAt(deadBody.transform);
-        //disableCasket();
-        disablePlayer1();
-        disablePlayer2();
-        disableBody();
-        StartCoroutine(displayIngameDialogue());
-
-        GameState.onGround = false;
+        
     }
 
 
@@ -99,110 +58,6 @@ public class GameManager : MonoBehaviour
         player2.SetActive(true);
         deadBody.SetActive(true);
     }
-
-    public void disablePlayer1(){
-        player1.GetComponent<Player1>().enabled = false;
-        player1.GetComponent<Animator>().enabled = false;
-    }
-
-    public void disablePlayer2(){
-        player2.GetComponent<Player2>().enabled = false;
-        player2.transform.GetChild(0).gameObject.GetComponent<Animator>().enabled = false;
-    }
-
-    public void disableCasket(){
-        casket.GetComponent<Casket>().enabled = false;
-        casket.GetComponent<Rigidbody>().isKinematic = true;
-        anchor.GetComponent<Anchor>().enabled = false;
-        anchor.GetComponent<Rigidbody>().isKinematic = true;
-    }
-
-    public void enableCasket(){
-        casket.GetComponent<Casket>().enabled = true;
-        casket.GetComponent<Rigidbody>().isKinematic = false;
-        anchor.GetComponent<Anchor>().enabled = true;
-        anchor.GetComponent<Rigidbody>().isKinematic = false;
-    }
-
-    public void disableBody(){
-        deadBody.transform.GetChild(1).gameObject.GetComponent<Collider>().enabled = false;
-        deadBody.transform.GetChild(1).gameObject.GetComponent<Deadbody>().enabled = false;
-    }
-
-    public IEnumerator displayIngameDialogue(){
-        p1dialogue.transform.position = new Vector3(player1.transform.position.x, player1.transform.position.y+5.0f, player1.transform.position.z);
-        p2dialogue.transform.position = new Vector3(player2.transform.position.x, player2.transform.position.y+5.0f, player2.transform.position.z);
-        yield return new WaitForSeconds(1.0f);
-        switch(GameState.life){
-            case 2:
-                
-                p1dialogue.GetComponent<TextMesh>().text = "...";
-                yield return new WaitForSeconds(1.0f);
-                p2dialogue.GetComponent<TextMesh>().text = "Shit.";
-                StartCoroutine(deathReset());
-                break;
-            case 1:
-                p1dialogue.GetComponent<TextMesh>().text = "One more time...";
-                yield return new WaitForSeconds(0.2f);
-                p2dialogue.GetComponent<TextMesh>().text = "We will be really screwed.";
-                StartCoroutine(deathReset());
-                break;
-            default:
-                break;
-        }
-    }
-
-    public IEnumerator deathReset(){
-        deathCanvas.SetActive(true);
-        casket.GetComponent<Rigidbody>().isKinematic = true;
-        yield return new WaitForSeconds(2.0f);
-
-        p1dialogue.GetComponent<TextMesh>().text = "";
-        p2dialogue.GetComponent<TextMesh>().text = "";
-
-        //reset position of objects
-        player1.transform.position = p1Pos;
-        player1.transform.rotation = Quaternion.Euler(0, 90, 0);
-
-        player2.transform.position = p2Pos;
-        player2.transform.rotation = Quaternion.Euler(0, 90, 0);
-
-        casket.transform.position = casPos;
-        casket.transform.rotation = Quaternion.Euler(0, 268, 0);
-
-        anchor.transform.position = anchorPos;
-        anchor.transform.rotation = Quaternion.Euler(0, 180, 0);
-
-        deadBody.transform.GetChild(1).gameObject.transform.position = bodyPos;
-        deadBody.transform.GetChild(1).localRotation = Quaternion.Euler(0, 0, 0);
-        deadBody.transform.GetChild(1).gameObject.GetComponent<Rigidbody>().isKinematic = true;
-        resetBodyRotation();
-        //reset Player 1 rotation
-
-        yield return new WaitForSeconds(0.5f);
-        deathCanvas.GetComponent<Animator>().Play("DeathResetFadeOut");
-        
-        
-        deadBody.transform.GetChild(1).gameObject.GetComponent<Collider>().enabled = true;
-        deadBody.transform.GetChild(1).gameObject.GetComponent<Deadbody>().enabled = true;
-        //enableCasket();
-        player1.GetComponent<Player1>().enabled = true;
-        player2.GetComponent<Player2>().enabled = true;
-        
-        yield return new WaitForSeconds(0.5f);
-        casket.GetComponent<Rigidbody>().isKinematic = false;
-        deadBody.transform.GetChild(1).gameObject.GetComponent<Rigidbody>().isKinematic = false;
-    }
-
-
-    public void resetBodyRotation(){
-        foreach(Transform child in deadBody.transform.GetChild(1)){
-            if(child.gameObject.GetComponent<Collider>()!=null){
-                child.localRotation = Quaternion.Euler(0, 0, child.eulerAngles.z);
-            }
-        }
-    }
-    
 
 
 }
